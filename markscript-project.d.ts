8ba147b220ff4ec849a0082e80d427b0834f1013
Christy Haragan <christy@christy.haragan@gmail.com>
2015-09-09 11:05:51 +0200

    Add mising dependencies, and fix type definition

--- markscript-project.d.ts
+++ markscript-project.d.ts
@@ -4,11 +4,6 @@ declare module "markscript-project/dist/lib/index"{
     export = tmp;
 }
 
-declare module "markscript-project/dist/lib/loaderPlugin"{
-    import tmp = require('dist/lib/loaderPlugin');
-    export = tmp;
-}
-
 declare module "markscript-project/dist/lib/runOptions"{
     import tmp = require('dist/lib/runOptions');
     export = tmp;
